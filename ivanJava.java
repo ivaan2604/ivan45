550afbb4695a1cae4cbd313737e5b63258c01732
ivan45 <ivan45@examen.local>
2023-03-02 10:06:48 +0000

    v2

--- ivanJava.java
+++ ivanJava.java
@@ -1,1 +1,8 @@
-Me llamo Ivan y he creado un servidor llamado Ivan45-servidor
+public class HolaMundo {
+
+	public static void main(String[] args) {		
+		System.out.println("Me llamo Ivan y he creado un servidor llamado Ivan45-servidor");
+		System.out.println("Vivo en San Fernano de Henares");
+	}
+
+
